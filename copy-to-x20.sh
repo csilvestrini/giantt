83ef0a5b02db34a955534deb5123d98318e95dd5
Cameron Silvestrini <csilvestrini@google.com>
2019-06-06 12:28:22 +1000

    x20 deploy script, including compilation step

--- copy-to-x20.sh
+++ copy-to-x20.sh
@@ -1,5 +1,13 @@
 #!/bin/bash
+<<<<<<< Updated upstream
 rm -rf /google/data/rw/users/cs/csilvestrini/www/arcs/giantt/*
+=======
+set -e
+tsc && ./node_modules/webpack/bin/webpack.js
+
+rm -rf /google/data/rw/users/cs/csilvestrini/www/arcs/giantt/*
+
+>>>>>>> Stashed changes
 mkdir  /google/data/rw/users/cs/csilvestrini/www/arcs/giantt/{dist,src}/
 cp index.html /google/data/rw/users/cs/csilvestrini/www/arcs/giantt/
 cp src/gantt-script.js /google/data/rw/users/cs/csilvestrini/www/arcs/giantt/src/
