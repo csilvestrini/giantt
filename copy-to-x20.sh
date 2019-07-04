d7eada441c901481ae3254df8e006a9eb7afeae5
Cameron Silvestrini <csilvestrini@google.com>
2019-07-04 15:38:58 +1000

    cp -v

--- copy-to-x20.sh
+++ copy-to-x20.sh
@@ -5,6 +5,6 @@ tsc && ./node_modules/webpack/bin/webpack.js
 rm -rf /google/data/rw/users/cs/csilvestrini/www/arcs/giantt/*
 
 mkdir  /google/data/rw/users/cs/csilvestrini/www/arcs/giantt/{dist,src}/
-cp index.html /google/data/rw/users/cs/csilvestrini/www/arcs/giantt/
-cp src/gantt-script.js /google/data/rw/users/cs/csilvestrini/www/arcs/giantt/src/
-cp dist/bundle.js /google/data/rw/users/cs/csilvestrini/www/arcs/giantt/dist/
+cp -v index.html /google/data/rw/users/cs/csilvestrini/www/arcs/giantt/
+cp -v src/gantt-script.js /google/data/rw/users/cs/csilvestrini/www/arcs/giantt/src/
+cp -v dist/bundle.js /google/data/rw/users/cs/csilvestrini/www/arcs/giantt/dist/
